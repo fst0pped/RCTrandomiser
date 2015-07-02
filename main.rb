321ca557a3d5994f0863072fd6cdb1a563a0d5ff
fst0pped <andy_dragonattack@hotmail.com>
2015-07-02 16:51:57 +0100

    Add to member list working. Pairings page added.

--- main.rb
+++ main.rb
@@ -6,24 +6,28 @@ get '/' do
 	slim :index
 end
 
+get '/pairings' do
+	slim :pairings
+end
+
 get '/membernames' do
 		slim :membernames
 end
 
 post '/' do
 	if params[:memberlist]
-		@memberlist = CSV.read(params[:memberlist][:tempfile])
+		$memberlist = CSV.read(params[:memberlist][:tempfile])
 		if params[:exclusionlist]
-			@exclusionlist = CSV.read(params[:exclusionlist][:tempfile])
+			$exclusionlist = CSV.read(params[:exclusionlist][:tempfile])
 		end
-	slim :membernames
+	slim :pairings
 	else slim :index
 	end
 end
 
 post '/membernames' do
 		if params[:newname]
-			@memberlist << params[:newname]
+			$memberlist << params[:newname]
 		end
 		slim :membernames
 end
